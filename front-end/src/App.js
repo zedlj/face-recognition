b769d213754aeccb2be216dff088a97527776d1b
zedlj <zoelauren-jones@hotmail.co.uk>
2020-05-14 11:06:55 +0100

    deplot front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-       fetch('http://localhost:3000/imageurl', {
+       fetch('https://vast-sierra-15377.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -93,7 +93,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response=> {
           if(response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://vast-sierra-15377.herokuapp.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
